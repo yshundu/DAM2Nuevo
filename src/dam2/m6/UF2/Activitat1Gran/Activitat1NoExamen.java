ecc9e1c8570834399227a344e9756e8d6f8f6803
Yang <Yang@DESKTOP-BTO7149>
2020-11-30 23:26:00 +0100

    Activitat 1 M6 Començada

---
+++ src/dam2/m6/UF2/Activitat1Gran/Activitat1NoExamen.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dam2.m6.UF2.Activitat1Gran;
+
+/**
+ *
+ * @author Yang
+ */
+public class Activitat1NoExamen {
+    
+}
