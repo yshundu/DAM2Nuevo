656e5efda22b30d615f5e443557eeb892cfd16be
Alumne <Alumne@AI104-PC12>
2021-01-26 19:54:33 +0100

    Apartat 1 activitat 4 UF2 M9 Acabada i comentada

--- src/dam2/m9/UF2/Activitat4/Act4Ap1.java
+++ src/dam2/m9/UF2/Activitat4/Act4Ap1.java
@@ -3,42 +3,34 @@ package dam2.m9.UF2.Activitat4;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 import java.util.concurrent.ThreadPoolExecutor;
 
 /**
  *
- * @author Alumne
+ * @author Yangshun Zhan Du i Raul Gimenez Perez
  */
 public class Act4Ap1 {
     public static void main(String[] args) throws InterruptedException {
-        ThreadPoolExecutor executor = (ThreadPoolExecutor) Executors.newFixedThreadPool(10);
+        //La quantitat de fills
+        ThreadPoolExecutor executor = (ThreadPoolExecutor) Executors.newFixedThreadPool(2);
+        //Guardem una llista amb les tasques
         List<Client> llistaTasques = new ArrayList<Client>();
-        
+        //Creem les 50 clients
         for (int i=1;i<=50;i++) {
+            //Aleatori de les quantitats i ho afegim al constructor
             int cantitatArticles = (int) (Math.random() * 30 + 1);
-            Client client = new Client(cantitatArticles);
+            Client client = new Client(cantitatArticles, i);
+            //Afegim la tasca
             llistaTasques.add(client);
-            System.out.println("Creat el client "+i+" amb " + client.getArticles() + " articles " + Arrays.toString(client.getArrayArticles()));
-            Thread.sleep(3000);
+           // System.out.println("Creat el client "+i+" amb " + client.getArticles() + " articles " + Arrays.toString(client.getArrayArticles()));            
+            
+            //executor.shutdown(); 
+            //Thread.sleep(3000);
         }
-        
         List <Future<Integer>> llistaResultats;
-	llistaResultats = executor.invokeAll(llistaTasques);
-        //executor.shutdown(); 
-        
-        for (int i = 0; i < llistaResultats.size(); i++) {
-	Future<Integer> resultat = llistaResultats.get(i);
-	try {
-            System.out.println("Resultat tasca "+i+ " és:" + resultat.get());
-	}
-	catch (Exception e)
-		{
-               e.printStackTrace();
-		}
-	}
+        llistaResultats = executor.invokeAll(llistaTasques);
     }
     
 }
--- src/dam2/m9/UF2/Activitat4/Client.java
+++ src/dam2/m9/UF2/Activitat4/Client.java
@@ -1,5 +1,6 @@
 package dam2.m9.UF2.Activitat4;
 
+import java.util.Arrays;
 import java.util.concurrent.Callable;
 
 /**
@@ -8,14 +9,26 @@ import java.util.concurrent.Callable;
  */
 public class Client implements Callable<Integer>{
     int articles;
+    int numClient;
     int arrayArticles[];
-
-    public Client(int articles) {
+ //Constructor
+    public Client(int articles, int numClient) {
         this.articles = articles;
+        this.numClient = numClient;
         arrayArticles = new int[articles];
         for (int i=0;i<articles;i++){
             arrayArticles[i] = (int) (Math.random() * 7 + 2);
         }
+        System.out.println("Creat el client "+ getNumClient() +" amb " + getArticles() + " articles " + Arrays.toString(getArrayArticles()));
+        
+    }
+
+    public int getNumClient() {
+        return numClient;
+    }
+
+    public void setNumClient(int numClient) {
+        this.numClient = numClient;
     }
     
         public int[] getArrayArticles() {
@@ -36,9 +49,24 @@ public class Client implements Callable<Integer>{
 
     @Override
     public Integer call() throws Exception {
-        for (int i=0;i<articles;i++){
-            System.out.println(arrayArticles[i]);
-        }
-        return 0;
+        System.out.println("Client " + getNumClient() + " passa per caixa...");
+        //int tempsAleatori;
+          for (int j = 0; j < articles + 1; j++) {
+                //tempsAleatori = (int) (Math.random() * (6 - 0 + 1));
+                try {
+                    Thread.sleep(getArrayArticles()[j] * 500);
+                }catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                if (j == articles) {
+                    //Si la tasca s'ha acabat
+                    System.out.println("Client " + numClient + " article " + j + "/" + articles + " (" + getArrayArticles()[j] + " segons) FINALITZAT");
+                }else{
+                    //Sino
+                    System.out.println("Client " + numClient + " article " + j + "/" + articles + " (" + getArrayArticles()[j] + " segons)");
+                }
+            }
+        return 1;
+        
     }
 }
