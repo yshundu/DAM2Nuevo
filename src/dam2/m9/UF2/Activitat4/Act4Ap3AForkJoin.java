2030e0c18f1564ec9817f14520e720626f2b6bf9
Yang <yshundu@gmail.com>
2021-02-01 11:26:29 +0100

    Act 4 Apartat 3 part A feta (ForkJoin)

---
+++ src/dam2/m9/UF2/Activitat4/Act4Ap3AForkJoin.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dam2.m9.UF2.Activitat4;
+
+import java.util.concurrent.ForkJoinPool;
+import java.util.concurrent.RecursiveTask;
+/**
+ *
+ * @author Yang
+ */
+public class Act4Ap3AForkJoin extends RecursiveTask<Long>{
+    int base;
+    int exponente;
+
+    public Act4Ap3AForkJoin(int base, int exponente) {
+        this.base = base;
+        this.exponente = exponente;
+    }
+    
+    public static int elevadoX(int base, int exponente) {
+        if (exponente == 0) {
+            return 1;
+        } else {
+        return elevadoX (base,exponente-1) * base;
+        }
+    }
+    
+    @Override
+    protected Long compute() {
+        if (exponente == 0) {
+            return (long) 1;
+        } else {
+            Act4Ap3AForkJoin tmp = new Act4Ap3AForkJoin(base,exponente-1);
+            tmp.fork();
+            return tmp.join() * base;
+        }
+    }
+    
+        public static void main(String[] args) {
+        ForkJoinPool pool = new ForkJoinPool();
+        System.out.println(pool.invoke(new Act4Ap3AForkJoin(5,5)));
+
+    }
+}
