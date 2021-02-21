3bc5ebd8e23e3c1be7739d86d5dc3bc384654ef3
Yang <yshundu@gmail.com>
2021-02-21 22:02:00 +0100

    M3 exemples

---
+++ src/dam2/m3/UF6/NoAvaluables/Activitat6.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dam2.m3.UF6.NoAvaluables;
+
+/**
+ *
+ * @author Alumne
+ */
+public class Activitat6 {
+    
+}
