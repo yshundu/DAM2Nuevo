7b495c1dba0fffd3b8e1622b5473fb7ad8f9b044
Alumne <Alumne@AI104-PC12>
2021-03-11 16:36:46 +0100

    Avanzando activitat 3

--- src/dam2/m6/UF3/Exercici3.java
+++ src/dam2/m6/UF3/Exercici3.java
@@ -5,8 +5,9 @@
  */
 package dam2.m6.UF3;
 
-import java.util.Collection;
-import org.hibernate.metamodel.relational.Database;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import org.xmldb.api.*;
 import org.xmldb.api.base.*;
 import org.xmldb.api.modules.*;
@@ -15,7 +16,7 @@ import org.xmldb.api.modules.*;
  * @author Alumne
  */
 public class Exercici3 {
-    public static void main (String[] args) {
+    public static void main (String[] args) throws XMLDBException {
         //Driver per a eXist
         String driver = "org.exist.xmldb.DatabaseImpl";
         //Col·lecció
@@ -26,7 +27,7 @@ public class Exercici3 {
         String usu = "admin";
         //Contrasenya
         String usuPass = "alumne";
-        
+        //Connexió a la base de dades
         try {
             //Carrega el driver
             Class cl = Class.forName(driver);
@@ -38,10 +39,26 @@ public class Exercici3 {
             System.out.println("Error en inicialitzar la base de dades eXist");
             e.printStackTrace();
         }
-        col = DatabaseManager.getCollection(URI, usu, usuPass);
-        if (col==null)
-            System.out.println("*** LA COL·LECCIÓ NO EXISTEIX ***");
         
+
+        //Conversió a numèric
+        
+        col = DatabaseManager.getCollection(URI, usu, usuPass);
+        
+        if (col==null) {
+            System.out.println("*** LA COL·LECCIÓ NO EXISTEIX ***");
+        }
+        System.out.println("Escriu un departament: ");
+        String s = null;
+        //Entrada estandard
+        try {
+            BufferedReader in = new BufferedReader (new InputStreamReader(System.in));
+            s = in.readLine();
+        } catch (IOException e) {
+            System.out.println("Error en llegir");
+            e.printStackTrace();
+        }
+        int dep = Integer.parseInt(s);
         XPathQueryService servei = (XPathQueryService) col.getService("XPathQueryService", "1.0");
         ResourceSet result = servei.query("for $em in /EMPLEADOS/EMP_ROW/[DEPT_NO=20] return $em");
         
@@ -50,8 +67,9 @@ public class Exercici3 {
         i = result.getIterator();
         if (!i.hasMoreResources())
             System.out.println("LA CONSULTA NO TORNA RES");
-        while (i.hasMoreResource()) {
-            Resource r = i.nextResource();
+        
+        while (i.hasMoreResources()) {
+           Resource r = i.nextResource();
             System.out.println((String)r.getContent());
         }
         //S'esborra
